da3c646277ded75752fd00d58bb682a86eeb83da
abdulrehman202 <abdulrehman1411@gmail.com>
2025-05-05 21:18:53 +0500

    message time in 12 hours

--- lib/View/chat_screen.dart
+++ lib/View/chat_screen.dart
@@ -8,8 +8,6 @@ class ChatScreen extends StatefulWidget {
   ChatRoom chatRoom =
       ChatRoom('abc', User('sabc', 'Sender'), User('rabc', 'Receiver'), []);
 
-    
-
   ChatScreen({super.key});
 
   @override
@@ -19,34 +17,68 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   TextEditingController _msgController = TextEditingController();
 
-  Widget messageBubble(bool isSender)
-  {
-    double radius = 20.0;
+  Widget messageBubble(Message message) {
+    DateTime msgTime = message.time;
+    double radius = 30.0;
     BoxDecoration senderDec = BoxDecoration(
-        color: Colors.orangeAccent,
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(radius),topRight: Radius.circular(radius),bottomLeft: Radius.circular(radius), ),
-      );
+      color: Colors.orangeAccent,
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(radius),
+        topRight: Radius.circular(radius),
+        bottomLeft: Radius.circular(radius),
+      ),
+    );
 
     BoxDecoration receiverDec = BoxDecoration(
-        color: const Color.fromARGB(255, 108, 208, 238),
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(radius),topRight: Radius.circular(radius),bottomRight: Radius.circular(radius), ),
-      );
-    String temp = '0jcacbdghcdsgvydsgyudsgcsgcbxhzcbvhzvch VCHcvycvs';
+      color: const Color.fromARGB(255, 108, 208, 238),
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(radius),
+        topRight: Radius.circular(radius),
+        bottomRight: Radius.circular(radius),
+      ),
+    );
     return Align(
-      
-          alignment: isSender?Alignment.centerRight:Alignment.centerLeft,
+      alignment: message.sender == widget.chatRoom.sender.id ? Alignment.centerRight : Alignment.centerLeft,
       child: Container(
         alignment: Alignment.centerLeft,
         padding: const EdgeInsets.all(10),
-        margin: const EdgeInsets.all(10),
-        decoration: isSender? senderDec:receiverDec,
-        child: Wrap(
+        margin: const EdgeInsets.only(bottom: 10,left: 10,right: 10),
+        decoration: message.sender == widget.chatRoom.sender.id  ? senderDec : receiverDec,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          
           children: [
-            Text('Sample messagef'+temp,overflow: TextOverflow.clip,),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                message.content,
+                overflow: TextOverflow.clip,
+              ),
+            ),
+            Container(
+                margin: const EdgeInsets.symmetric(vertical: 5),
+                alignment: Alignment.bottomRight,
+                child: Text( get12hrsTime(msgTime) //'${msgTime.hour.toString().padLeft(2,'0')}:${msgTime.minute.toString().padLeft(2,'0')}'
+                ))
           ],
         ),
       ),
     );
+  }
+
+  String get12hrsTime(DateTime dtime)
+  {
+    String time = '';
+    String unit = dtime.hour>12 ?'pm':'am';
+    String min = dtime.minute.toString(), hour = dtime.hour.toString();
+    
+    if(dtime.hour>12)
+    {
+      hour = (dtime.hour-12).toString();
+    }
+
+    time = '$hour:$min $unit';
+    return time;
   }
 
   Widget messageWriter() {
@@ -56,31 +88,31 @@ class _ChatScreenState extends State<ChatScreen> {
         child: Row(
           children: [
             Expanded(
-              flex: 9,
+                flex: 9,
                 child: Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Colors.white,
-              ),
-              child: TextField(
-                maxLines: null,
-                keyboardType: TextInputType.multiline,
-                decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.all(20),
-                    hintText: 'Write a message...',
-                    border: InputBorder.none),
-                controller: _msgController,
-              ),
-            )),
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Colors.white,
+                  ),
+                  child: TextField(
+                    maxLines: null,
+                    keyboardType: TextInputType.multiline,
+                    decoration: const InputDecoration(
+                        contentPadding: EdgeInsets.all(20),
+                        hintText: 'Write a message...',
+                        border: InputBorder.none),
+                    controller: _msgController,
+                  ),
+                )),
             Expanded(
-              flex: 1,
+                flex: 1,
                 child: Center(
                   child: IconButton(
                     onPressed: sendMsg,
                     icon: const Icon(
                       Icons.send,
-                      color: Colors.blue,
+                      color: Color.fromARGB(255, 158, 46, 46),
                     ),
                   ),
                 ))
@@ -90,7 +122,7 @@ class _ChatScreenState extends State<ChatScreen> {
 
   @override
   Widget build(BuildContext context) {
-    DateTime currentTime  = DateTime.now();
+    DateTime currentTime = DateTime.now();
     return Scaffold(
       resizeToAvoidBottomInset: false,
       bottomNavigationBar: Column(
@@ -107,26 +139,27 @@ class _ChatScreenState extends State<ChatScreen> {
           widget.chatRoom.receiver.name,
           style: const TextStyle(color: Colors.white),
         ),
-        backgroundColor: Colors.blue,
+        backgroundColor: const Color.fromARGB(255, 158, 46, 46),
       ),
       body: SingleChildScrollView(
-      child: Column(
-        children: [
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.all(10),
-            child:  Text('${currentTime.day.toString()} ${Constants.getMonth(currentTime.month)} ${currentTime.year.toString()}')),
-          messageBubble(true),
-          messageBubble(false),
-        ],
+        child: Column(
+          children: [
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                    '${currentTime.day.toString()} ${Constants.getMonth(currentTime.month)} ${currentTime.year.toString()}')),
+            messageBubble(Message('abc', 'Hello!', DateTime.now(), 'sabc', 'rabc')),
+            messageBubble(Message('abc', 'Hi!', DateTime.now(), 'rabc', 'sabc')),
+            messageBubble(Message('abc', 'How you doing?', DateTime.now(), 'sabc', 'rabc')),
+            
+          ],
+        ),
       ),
-    ),
     );
   }
 
   void sendMsg() {
     _msgController.clear();
   }
-
-  
 }
